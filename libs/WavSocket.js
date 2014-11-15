2b7fced36d7182fabd665e5c48abb2a3c51a5533
xlune <ichikawa@xlune.com>
2014-11-15 22:43:18 +0900

    fix #13

--- libs/WavSocket.js
+++ libs/WavSocket.js
@@ -4,7 +4,7 @@ var app = require('http').createServer(function handler (req, res) {
 	var urlinfo = require('url').parse( req.url , true);
 	if(urlinfo.pathname.match(/\/tmp\/[a-z0-9_]+\.wav$/)){
 		path = urlinfo.pathname;
-	}else if(urlinfo.pathname.match(/\.(html|js)$/)){
+	}else if(urlinfo.pathname.match(/\.(html|js|css)$/)){
 		path = __dirname + '/../www' + urlinfo.pathname;
 	}else{
 		path = __dirname + '/../www/index.html';
