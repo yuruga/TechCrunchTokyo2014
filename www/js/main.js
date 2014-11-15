7439432a5d73fd2a57e578b4f97c760de6b8098f
maeda <maeda@taki-mex.jp>
2014-11-16 01:14:38 +0900

    no message

--- www/js/main.js
+++ www/js/main.js
@@ -1,38 +1,3 @@
-/*var API_HOST = "";
-var API_VER = "";
-var API_END_POINT_READ = API_HOST + "/read";
-
-var face = document.getElementBuId("#face");
-var audio = document.getElementBuId("#face");
-
-function init(){
-	face.addEventListener('webkitAnimationEnd', function(){
-		this.style.webkitAnimationName = '';
-	}, false);
-}
-
-function playAnim(id)
-{
-
-}
-
-
-
-function playAudio(path){
-	audio.src = path;
-	player.onload = function(e){
-		console.log("success", e);
-		audio.play();
-	};
-	player.onerror = function(e){
-		console.log("error", e);
-	};
-	audio.load();
-}*/
-//face init
-
-
-
 
 //socket init
 var opt = {
@@ -51,8 +16,8 @@ socket.on('play', function(e){
     var face = document.getElementById("face");
     var player = document.getElementById("audio");
     //
-    animId = e.animId;
-    audioPath = e.audioPath;
+    animId = e.mode;
+    audioPath = e.path;
 
     
 
@@ -69,7 +34,7 @@ socket.on('play', function(e){
     player.load();
 
     //play face
-    face.set
+    face.className = animId;
 
 
 
@@ -81,4 +46,15 @@ function playFaceAnim(animId){
         this.style.webkitAnimationName = '';
     }, false);*/
     face.className = animId;
+    onResize();
+}
+
+window.addEventListener('resize',onResize);
+
+var FRAME_WIDTH = 2043;
+function onResize(e){
+    var face = document.getElementById("face");
+    p = window.outerWidth/FRAME_WIDTH;
+    console.log("aaa",window.width,window);
+    face.style["transform"] = "scale("+p+")";
 }
